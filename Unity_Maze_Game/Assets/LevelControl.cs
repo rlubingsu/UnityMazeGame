9af74385b2d5d22dd1278bdf021289b4515b82aa
thePrd1 <34972323+thePrd1@users.noreply.github.com>
2019-06-14 13:32:28 -0400

    Files

---
+++ Unity_Maze_Game/Assets/LevelControl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelControl : MonoBehaviour {
+
+	// Use this for initialization
+ 
+
+    private void OnTriggerEnter2D(Collider other)
+    {
+        if (!other.CompareTag("Player"))
+        {
+           // SceneManager.LoadScene(1);
+            SceneManager.LoadScene("Level1");
+        }
+    }
+}
---
+++ Unity_Maze_Game/Assets/ProceduralNumberGenerator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class ProceduralNumberGenerator {
+	public static int currentPosition = 0;
+	public const string key = "123424123342421432233144441212334432121223300";
+
+	public static int GetNextNumber() {
+		string currentNum = key.Substring(currentPosition++ % key.Length, 1);
+
+        //return Random.Range(1,5); 
+		return int.Parse (currentNum);
+	}
+
+  
+
+}
